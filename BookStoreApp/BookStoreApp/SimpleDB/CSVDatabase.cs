e16bfb620eefe04f2b695c8c6bfb747d192009cf
Mads Orfelt <118978052+H4xx0r9@users.noreply.github.com>
2023-11-05 13:09:11 +0100

    Add files via upload

---
+++ BookStoreApp/BookStoreApp/SimpleDB/CSVDatabase.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Globalization;
+using CsvHelper;
+
+namespace BookStoreApp.SimpleDB
+{
+    public sealed class CSVDatabase : IDatabaseRepository<Cheeps>
+    {
+        public readonly string CsvFilePath;
+
+        public CSVDatabase(string filePath)
+        {
+            CsvFilePath = filePath;
+        }
+
+        public IEnumerable<Cheeps> Read(int? limit = null)
+        {
+            using var sr = new StreamReader(CsvFilePath);
+            using var csv = new CsvReader(sr, CultureInfo.InvariantCulture);
+            var records = csv.GetRecords<Cheeps>().ToList();
+
+            if (limit.HasValue)
+            {
+                records = records.Take(limit.Value).ToList();
+            }
+
+            return records;
+        }
+
+        public void Store(Cheeps record)
+        {
+            using var writer = new StreamWriter(CsvFilePath, true);
+            using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            
+            csv.WriteRecord(record);
+            csv.Flush();
+            writer.WriteLine();
+        }
+    }
+}
---
+++ BookStoreApp/BookStoreApp/SimpleDB/Cheeps.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStoreApp;
+
+public record Cheeps
+{
+    public string Author { get; set; } = "";
+    public string Message { get; set; } = "";
+    public DateTime Timestamp { get; set; }
+}
